7f5d8b329a3b24a3aaa1bd8f2c5b10a0867b848f
maryRom01 <tester.automation.11@gmail.com>
2024-10-25 11:29:01 -0400

    add html

---
+++ layouts/login_form/script.js
@@ -0,0 +1,17 @@
+function showSignup() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("signupForm").style.display = "block";
+}
+
+function showLogin() {
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("signupForm").style.display = "none";
+
+  function login() {
+
+  }
+
+  function signup() {
+    
+  }
+}
