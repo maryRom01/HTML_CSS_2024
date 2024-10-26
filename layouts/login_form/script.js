9b25335198c4c3e85b29776e06961cbd52010516
maryRom01 <tester.automation.11@gmail.com>
2024-10-25 22:41:01 -0400

    add nav buttons styles (login, signup)

--- layouts/login_form/script.js
+++ layouts/login_form/script.js
@@ -1,17 +1,19 @@
+function showLogin() {
+  document.getElementById("loginForm").style.display = "block";
+  document.getElementById("signupForm").style.display = "none";
+}
+
 function showSignup() {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("signupForm").style.display = "block";
 }
 
-function showLogin() {
-    document.getElementById("loginForm").style.display = "block";
-    document.getElementById("signupForm").style.display = "none";
 
-  function login() {
 
-  }
+function login() {
 
-  function signup() {
-    
-  }
+}
+
+function signup() {
+  
 }
